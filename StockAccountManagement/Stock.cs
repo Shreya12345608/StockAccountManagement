c1c9cf544e368ab378b8da2686347bf3185d4411
Shreya Malviya <malviyashreya26@gmail.com>
2021-06-11 16:42:22 +0530

    [Shreya] Add: Stock Account Management

---
+++ StockAccountManagement/Stock.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    class Stock
+    {
+
+        // each Stock has properties like name, number od shares and share price.
+        // this stocks are stored in
+
+        public string stockName { get; set; }
+        public int numberOfStocks { get; set; }
+        public float stockPrice { get; set; }
+
+        public static List<Stock> stockList = new List<Stock>();
+
+        // View the Stocks available in the Market
+
+        public void StockView()
+        {
+            try
+            {
+                //float totalStockvalue =0 
+                float totalStockValue = 0;
+                float valueOfStock;
+                Console.WriteLine();
+                Console.WriteLine("\t\t\tStock Market");
+                Console.WriteLine();
+                Console.WriteLine("Stock Name   Number of Shares   Share Price in .Rs  Value of Stock in.Rs");
+                //foreach loop 
+                foreach (Stock st in stockList)
+                {
+                    Console.WriteLine();
+                    //print Op value
+                    Console.WriteLine($"{st.stockName}\t\t{st.numberOfStocks}\t\t\t{st.stockPrice} \t\t" +
+                        $"{valueOfStock = ValueOfStock(st.stockPrice, st.numberOfStocks)}");
+                    // Increment totalstockvalue with total value
+                    totalStockValue += TotalValue(valueOfStock);
+                }
+                Console.WriteLine("---------------------------------------------------------------------");
+                //OP total stock value
+                Console.WriteLine($"Total Stock Value(.Rs)\t\t\t\t\t{totalStockValue}");
+            }
+            // for error 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        // methods to calculate total value and value of each stocks
+
+        public float ValueOfStock(float shareprice, int numberOfShares)
+        {
+            float valueOfStock = shareprice * numberOfShares;
+            return valueOfStock;
+        }
+        // methods to calculate total value and value
+        public float TotalValue(float valueOfStock)
+        {
+            float totalValueOfStocks = +valueOfStock;
+            return totalValueOfStocks;
+        }
+    }
+}
---
+++ StockAccountManagement/StockPortFolio.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StockAccountManagement
+{
+    class StockPortFolio
+    {
+
+        // Managing Portfolio of person holding stocks of various companies
+
+        public string StockHolder { get; set; }
+        public string[] portStockName { get; set; }
+        public int[] portNumberOfShare { get; set; }
+        public float[] protStockPrice { get; set; }
+
+        public static List<StockPortFolio> stockPortfolioList = new List<StockPortFolio>();
+        Stock stock = new Stock();
+
+        /// <summary>
+        /// view format for portfolio,
+        /// calculating value of each stock and total value of stocks.
+        /// </summary
+
+        public void PortFolioView()
+        {
+            try
+            {
+                int i = 0;
+                float portValueOfStocks, portTotalValue = 0;
+                Console.WriteLine();
+                foreach (StockPortFolio sp in stockPortfolioList)
+                {
+                    Console.WriteLine("PortFolio of " + sp.StockHolder);
+                    Console.WriteLine("Stock name\tNumber of Shares\tValue of Stocks(.Rs)");
+                    while (i <= 4)
+                    {
+                        Console.WriteLine();
+                        Console.Write(sp.portStockName[i] + "\t\t  ");
+                        Console.Write(sp.portNumberOfShare[i] + "\t\t\t  ");
+                        portValueOfStocks = stock.ValueOfStock(sp.protStockPrice[i], sp.portNumberOfShare[i]);
+                        Console.WriteLine(portValueOfStocks);
+                        portTotalValue += stock.TotalValue(portValueOfStocks);
+                        i++;
+                    }
+                    Console.WriteLine("---------------------------------------------------------------");
+                    Console.WriteLine($"Total Value(.Rs)\t\t\t  {portTotalValue} ");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// using streamwriter for writing details to file from list
+        /// </summary>
+        public void WriteFile()
+        {
+            //file path for writing and reading details
+            try
+            {
+                string filepath = @"C:\Users\hp\Desktop\BriLabz\StockAccountManagement\StockAccountManagement\StockReport.txt";
+
+                using (StreamWriter streamWriter = new StreamWriter(filepath))
+                {
+                    int i = 0;
+                    foreach (StockPortFolio st in stockPortfolioList)
+                    {
+                        streamWriter.Write(st.StockHolder + "\t" + st.portStockName[i] + "\t" + st.portNumberOfShare[i]);
+                        //   streamWriter.Write(st.portStockName[i] + "\t" + st.portNumberOfShare[i]);
+                        //while (i <= 4)
+                        //    streamWriter.Write(st.StockHolder);
+                        //streamWriter.Write(st.portStockName[i] + "\t" + st.portNumberOfShare[i]);
+                        //i++;
+                    }
+
+                    streamWriter.Close();
+      
+
+        }
+               
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+          
+            
+
+        }
+        // streamWriter.WriteLine(StockHolder);
+        //int i = 0;
+
+    }
+}
